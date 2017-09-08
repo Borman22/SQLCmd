f3f8a9128ace373dcaa33db8a05177b992c4d2b0
Borman22 <borman22@ukr.net>
2017-09-08 18:06:21 +0300

    Исправил тест Insert_Test. Тест проходит.

--- src/test/java/ua/borman/sqlcmd/controller/commands/DropDB_Test.java
+++ src/test/java/ua/borman/sqlcmd/controller/commands/DropDB_Test.java
@@ -1,62 +1,75 @@
-//package ua.borman.sqlcmd.controller.commands;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//import ua.borman.sqlcmd.controller.CommandExecutor;
-//import ua.borman.sqlcmd.model.DatabaseManager;
-//import ua.borman.sqlcmd.view.Console;
-//
-//import java.sql.Connection;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.sql.Statement;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.fail;
-//import static ua.borman.sqlcmd.Templates_For_Tests.*;
-//
-//public class DropDB_Test {
-//
-//    @Before
-//    public void init() {
-//        DatabaseManager dbm = new DatabaseManager();
-//        CommandExecutor executor = getCommandExecutor(new Console(), dbm);
-//        executor.execute(getQueryString("createdb", DB_NAME_FALSE, USERNAME, PASSWORD));
-//        executor.execute("close");
-//    }
-//
-//    @Test
-//    public void dropDBTest() {
-//        DatabaseManager dbm = new DatabaseManager();
-//        CommandExecutor executor = getCommandExecutor(new Console(), dbm);
-//        executor.execute(getQueryString("connect", "", USERNAME, PASSWORD));
-//
-//        Connection connection = connectToSQL(DB_NAME_FALSE);
-//        try {
-//            if(connection == null || connection.isClosed()){
-//                fail("База данных не создана - удалять нечего");
-//                return;
-//            }
-//        } catch (SQLException e) {
-//            // Do nothing
-//        }
-//        try {
-//            connection.close();
-//        } catch (SQLException e) {
-//            System.err.println("Не удалось отключитьс от БД");
-//        }
-//        executor.execute(getQueryString("dropDB", DB_NAME_FALSE));
-//
-//        connection = connectToSQL(DB_NAME_FALSE);
-//        try {
-//            System.out.println(connection);
-//            if(connection != null && !connection.isClosed()){
-//                fail("База не удалилась - удалось к ней подключитсья");
-//                disconnect(connection);
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//        executor.execute("close");
-//    }
-//}
+package ua.borman.sqlcmd.controller.commands;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import ua.borman.sqlcmd.controller.CommandExecutor;
+import ua.borman.sqlcmd.model.DatabaseManager;
+import ua.borman.sqlcmd.view.Console;
+import ua.borman.sqlcmd.view.View;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static ua.borman.sqlcmd.Templates_For_Tests.*;
+
+public class DropDB_Test {
+
+
+    private static Connection connection;
+    private static DatabaseManager dbm;
+    private static View view;
+    private static CommandExecutor executor;
+
+    @BeforeClass
+    public static void init(){
+        dbm = new DatabaseManager();
+        view = new Console();
+        executor = new CommandExecutor(view, dbm);
+    }
+
+    @Before
+    public void init2() {
+        executor.execute(getQueryString("createdb", DB_NAME_FALSE, USERNAME, PASSWORD));
+        executor.execute("close");
+    }
+
+    @Test
+    public void dropDBTest() {
+        DatabaseManager dbm = new DatabaseManager();
+        CommandExecutor executor = getCommandExecutor(new Console(), dbm);
+        executor.execute(getQueryString("connect", "", USERNAME, PASSWORD));
+
+        Connection connection = connectToSQL(DB_NAME_FALSE);
+        try {
+            if(connection == null || connection.isClosed()){
+                fail("База данных не создана - удалять нечего");
+                return;
+            }
+        } catch (SQLException e) {
+            // Do nothing
+        }
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            System.err.println("Не удалось отключитьс от БД");
+        }
+        executor.execute(getQueryString("dropDB", DB_NAME_FALSE));
+
+        connection = connectToSQL(DB_NAME_FALSE);
+        try {
+            System.out.println(connection);
+            if(connection != null && !connection.isClosed()){
+                fail("База не удалилась - удалось к ней подключитсья");
+                disconnect(connection);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        executor.execute("close");
+    }
+}
--- src/test/java/ua/borman/sqlcmd/controller/commands/Insert_Test.java
+++ src/test/java/ua/borman/sqlcmd/controller/commands/Insert_Test.java
@@ -1,67 +1,76 @@
-//package ua.borman.sqlcmd.controller.commands;
-//
-//import org.junit.Before;
-//import org.junit.BeforeClass;
-//import org.junit.Test;
-//import ua.borman.sqlcmd.controller.CommandExecutor;
-//import ua.borman.sqlcmd.model.DatabaseManager;
-//import ua.borman.sqlcmd.view.Console;
-//
-//import java.sql.Connection;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.sql.Statement;
-//
-//import static org.junit.Assert.assertEquals;
-//import static ua.borman.sqlcmd.Templates_For_Tests.*;
-//
-//public class Insert_Test {
-//
-//    @BeforeClass
-//    public static void init(){
-//        createdbWithoutTables();
-//    }
-//
-//    @Before
-//    public void initTableWith3Rows(){
-//        createTablesAnd3Rows();
-//    }
-//
-//    @Test
-//    public void insertTest(){
-//        DatabaseManager dbm = new DatabaseManager();
-//        CommandExecutor executor = getCommandExecutor(new Console(), dbm);
-//        executor.execute(getQueryString("connect", DB_NAME, USERNAME, PASSWORD));
-//
-//        Connection connection = connectToSQL(DB_NAME);
-//        Statement statement = null;
-//        int countOfRows1 = 0;
-//        ResultSet rs;
-//        try {
-//            statement = connection.createStatement();
-//            rs = statement.executeQuery("SELECT COUNT(*) FROM \"" + TEST_TABLE + "\"");
-//            rs.next();
-//            countOfRows1 = rs.getInt(1);
-//        } catch (SQLException e) {
-//            System.err.println("Не могу получить стейтмент");
-//            System.err.println(e.getLocalizedMessage());
-//        }
-//
-//        executor.execute(getQueryString("insert", TEST_TABLE, "col1", "rN_c1_value", "col2", "rN_c2_value"));
-//
-//        int countOfRows2 = -100;
-//        try {
-//            rs = statement.executeQuery("SELECT COUNT(*) FROM \"" + TEST_TABLE + "\"");
-//            rs.next();
-//            countOfRows2 = rs.getInt(1);
-//        } catch (SQLException e) {
-//            System.err.println("Опять не могу получить стейтмент");
-//            e.printStackTrace();
-//        }
-//
-//        assertEquals(countOfRows1, countOfRows2 - 1);
-//
-//        disconnect(connection);
-//        executor.execute("close");
-//    }
-//}
+
+package ua.borman.sqlcmd.controller.commands;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import ua.borman.sqlcmd.controller.CommandExecutor;
+import ua.borman.sqlcmd.model.DatabaseManager;
+import ua.borman.sqlcmd.view.Console;
+import ua.borman.sqlcmd.view.View;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.Assert.assertEquals;
+import static ua.borman.sqlcmd.Templates_For_Tests.*;
+
+public class Insert_Test {
+
+    private static DatabaseManager dbm;
+    private static View view;
+    private static CommandExecutor executor;
+
+    @BeforeClass
+    public static void init(){
+        dbm = new DatabaseManager();
+        view = new Console();
+        executor = new CommandExecutor(view, dbm);
+        createdbWithoutTables(dbm, view);
+    }
+
+    @Before
+    public void initTableWith3Rows(){
+        createTablesAnd3Rows(dbm, view);
+    }
+
+    @Test
+    public void insertTest(){
+
+        executor.execute(getQueryString("connect", DB_NAME, USERNAME, PASSWORD));
+
+        Connection connection = connectToSQL(DB_NAME);
+        Statement statement = null;
+        int countOfRows1 = 0;
+        ResultSet rs;
+        try {
+            statement = connection.createStatement();
+            rs = statement.executeQuery("SELECT COUNT(*) FROM \"" + TEST_TABLE + "\"");
+            rs.next();
+            countOfRows1 = rs.getInt(1);
+        } catch (SQLException e) {
+            System.err.println("Не могу получить стейтмент");
+            System.err.println(e.getLocalizedMessage());
+        }
+
+        executor.execute(getQueryString("insert", TEST_TABLE, "col1", "rN_c1_value", "col2", "rN_c2_value"));
+
+        int countOfRows2 = -100;
+        try {
+            rs = statement.executeQuery("SELECT COUNT(*) FROM \"" + TEST_TABLE + "\"");
+            rs.next();
+            countOfRows2 = rs.getInt(1);
+        } catch (SQLException e) {
+            System.err.println("Опять не могу получить стейтмент");
+            e.printStackTrace();
+        }
+
+        assertEquals(countOfRows1, countOfRows2 - 1);
+
+        disconnect(connection);
+        executor.execute("close");
+    }
+}
+
