9acc154f4fed5dd98f72aad67b0db4e4efaad352
Borman22 <borman22@ukr.net>
2017-09-08 15:45:15 +0300

    Исправил тест Close_Test. Тест проходит.

--- src/test/java/ua/borman/sqlcmd/controller/commands/Close_Test.java
+++ src/test/java/ua/borman/sqlcmd/controller/commands/Close_Test.java
@@ -1,25 +1,39 @@
-//package ua.borman.sqlcmd.controller.commands;
-//
-//import org.junit.Test;
-//import ua.borman.sqlcmd.controller.CommandExecutor;
-//import ua.borman.sqlcmd.model.DatabaseManager;
-//import ua.borman.sqlcmd.view.Console;
-//
-//import static org.junit.Assert.assertFalse;
-//import static org.junit.Assert.assertTrue;
-//import static ua.borman.sqlcmd.Templates_For_Tests.*;
-//
-//public class Close_Test {
-//
-//    @Test
-//    public void closeTest(){
-//
-//        DatabaseManager dbm = new DatabaseManager();
-//        CommandExecutor executor = getCommandExecutor(new Console(), dbm);
-//        executor.execute(getQueryString("connect", "", USERNAME, PASSWORD));
-//        assertTrue(dbm.isConnected());
-//
-//        executor.execute("close");
-//        assertFalse(dbm.isConnected());
-//    }
-//}
+package ua.borman.sqlcmd.controller.commands;
+
+import org.junit.Before;
+import org.junit.Test;
+import ua.borman.sqlcmd.controller.CommandExecutor;
+import ua.borman.sqlcmd.model.DatabaseManager;
+import ua.borman.sqlcmd.view.Console;
+import ua.borman.sqlcmd.view.View;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static ua.borman.sqlcmd.Templates_For_Tests.*;
+
+public class Close_Test {
+
+    private static DatabaseManager dbm;
+    private static View view;
+    private static CommandExecutor executor;
+
+    @Before
+    public void init(){
+        dbm = new DatabaseManager();
+        view = new Console();
+        executor = new CommandExecutor(new Console(), dbm);
+    }
+
+    @Test
+    public void closeTest(){
+
+        executor.execute(getQueryString("connect", "", USERNAME, PASSWORD));
+        assertTrue(dbm.isConnected());
+
+        if(dbm.isConnected()) {
+            executor.execute("close");
+            assertFalse(dbm.isConnected());
+        }
+    }
+}
